df342ba73059ab959174cffa9c727f27f16a3941
최미래 <miraechoi@dgsw.hs.kr>
2024-03-11 20:38:49 +0900

    :: 변수

---
+++ src/main/kotlin/variable/variable.kt
@@ -0,0 +1,12 @@
+package variable
+
+fun main() {
+    // var/var1 변수명: 변수타입 = 초기화값
+    var name: String = "Mirae Choi"
+    name = "MIRAE CHOI"
+    val age: Int = 18
+    val alive: Boolean = true
+    val `in`: Int = 10
+    val first1: Int = 0
+
+}

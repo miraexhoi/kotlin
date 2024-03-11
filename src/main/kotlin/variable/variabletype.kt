67259266c21fd088eddbe53e81e6f5b7b9fd9ac4
최미래 <miraechoi@dgsw.hs.kr>
2024-03-11 23:48:56 +0900

    :: 변수 타입 [정수형]

---
+++ src/main/kotlin/variable/variabletype.kt
@@ -0,0 +1,12 @@
+package variable
+
+// var/var1 변수명: 변수타입 = 초기화값
+
+fun main() {
+    // 정수형 (Integer)
+    val one: Int = 1
+    val threeBillion: Long = 3000000000
+    val oneLong = 1L
+    /* => val oneLong: Long = 1 */
+    val oneByte: Byte = 1
+}

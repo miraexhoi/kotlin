7379b01d655bd3688aca7202f351a5c627232f51
최미래 <miraechoi@dgsw.hs.kr>
2024-03-11 20:30:58 +0900

    :: 기본 입출력 및 주석

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,6 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    // 어려운 로직에 대한 설명입니다. (한줄주석)
+    /* 모두 주석 처리합니다. */
+    println("Hello Kotlin#1")
+    print("Hello Kotlin#2")
 }

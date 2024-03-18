adcf2c645865416258b39bb72daf6ecf410da325
최미래 <miraechoi@dgsw.hs.kr>
2024-03-19 08:44:09 +0900

    :: 변수 연습 [변수, 변수타입 관련]

---
+++ src/main/kotlin/variable/Practice1.kt
@@ -0,0 +1,19 @@
+package variable
+
+fun main() {
+
+    /* 자신의 이름, 나이, 이메일 주소를 변수로 선언하고 출력해 주세요 */
+
+    val name1: String = "Mirae"
+    val age1: Int = 18
+    val email: String = "miraexhoi@gmail.com"
+    println("이름은 ${name1}이고 나이는 ${age1}살이고 이메일 주소는 ${email}입니다.")
+
+    /* 적절한 변수타입을 넣어주세요 */
+
+    val age2: Int = 10
+    val pi: Double = 3.1415
+    val isPossible: Boolean = true
+    val constructionCosts: Long = 5_000_000_000
+    val name2: String = "Mirae"
+}

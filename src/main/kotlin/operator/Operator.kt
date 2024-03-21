66c9a41ad9ebc1fd68ea6bcc62bac9637c5638fe
최미래 <miraechoi@dgsw.hs.kr>
2024-03-21 11:33:21 +0900

    :: 연산자 [증감 연산자, 비교 연산자, 논리 연산자]

--- src/main/kotlin/operator/Operator.kt
+++ src/main/kotlin/operator/Operator.kt
@@ -14,7 +14,7 @@ fun main() {
     println(a / b)
     println(a % b)
 
-    /* 1. 복합 대입 연산자
+    /* 2. 복합 대입 연산자
      * +=,-=,*=,/=,&
      * */
 
@@ -29,4 +29,45 @@ fun main() {
     println("c = $c")
     c %= 3
     println("c = $c")
+
+    /* 3. 증감 연산자
+     * ++, --
+     * */
+
+    var value:Int = 10
+    value++
+    println("value = $value")
+    value--
+    println("value = $value")
+
+    /* 4. 비교 연산자
+     * <, <=, >, >=, ==, !=
+     * */
+
+    var d: Int = 3
+    var e: Int = 3
+
+    println(a < b)
+    println(a <= b)
+    println(a > b)
+    println(a >= b)
+    println(a == b)
+    println(a != b)
+
+    /* 5. 논리 연산자
+     * ||(OR 연산자), &&(AND 연산자), !(NOT 연산자)
+     * */
+
+    println(true || true)
+    println(false || true)
+    println(true || false)
+    println(false || false)
+
+    println(true && true)
+    println(false && true)
+    println(true && false)
+    println(false && false)
+
+    println(!true)
+    println(!false)
 }
